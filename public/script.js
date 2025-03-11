c01d8d56d2aaa16eba554f3e266f25c5733d450c
annalounamaria <annalounamaria@gmail.com>
2025-03-11 17:04:01 +0200

    Viides commit

--- public/script.js
+++ public/script.js
@@ -2,4 +2,6 @@ document.getElementById("new-game-btn").addEventListener('click',startGame);
 
 function startGame(){
     console.log("KLIKATTU");
+    document.getElementById("intro-screen").style.display='none';
+    document.getElementById("game-screen").style.display='block';
 }
